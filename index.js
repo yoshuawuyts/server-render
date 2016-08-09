26dd392a42a9d8acab4b4949a9b61613cd335a33
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-08-09 17:06:59 +0200

    index: implement

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+const hyperstream = require('hyperstream')
+const index = require('simple-html-index')
 const assert = require('assert')
 
 module.exports = serverRender
 
 // HTML server rendering middleware
-// null -> null
-function serverRender () {
+// (obj?, fn(str) -> str) -> fn(obj, obj, fn)
+function serverRender (opts, handler) {
+  if (!handler) {
+    handler = opts
+    opts = {}
+  }
+
+  assert.equal(typeof opts, 'object', 'server-render: opts should be a function')
+  assert.equal(typeof handler, 'function', 'server-render: handler should be a function')
+
+  // call middlewarereadme
+  // (obj, obj, fn) -> null
+  return function (req, res, next) {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
+    const html = handler(req.url)
+    const hs = hyperstream({ 'body': { _appendHtml: html } })
+
+    index(opts).pipe(hs).pipe(res)
+  }
 }
